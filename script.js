88ee8e68ff65815694fa4b77260561e5cd7c63d3
kytaec <vladislavtokovenko4@gmail.com>
2024-06-01 14:36:15 +0300

    added word guessing

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ import {words} from './words.js'
 const wordsList = words.split('\n').map(word => word.trim()).filter(word => word.length > 0);
 const word = wordsList[Math.floor(Math.random() * wordsList.length)]
 const guessWord = document.getElementById('guess-word')
+const guessWordDisplay = document.getElementById('guess-word-display')
 
 // getting html elements
 const hangmanImg = document.getElementById('hangman-img')
@@ -14,8 +15,8 @@ const guessWordArray = word.split('')
 
 let score = 0
 let usedLettersArray = []
+let secretWord = '-'.repeat(word.length);
 
-guessWord.innerHTML = word
 
 function gameOver() {
     if(score === 7){
@@ -24,9 +25,12 @@ function gameOver() {
     }
 }
 
+guessWord.innerHTML = word
+guessWordDisplay.innerHTML = secretWord
+
 document.addEventListener("keydown", (event) => {
     const key = event.code.slice(3,4).toLowerCase()
-    if (usedLettersArray.indexOf(key) === -1){
+    if (usedLettersArray.indexOf(key) === -1) {
         usedLettersArray.push(key)
         const usedLettersStr = usedLettersArray.join(', ')
         usedLetters.innerHTML = usedLettersStr
@@ -35,6 +39,19 @@ document.addEventListener("keydown", (event) => {
         score++
         hangmanImg.setAttribute("src", `./images/hangMan-images/hang-${score}.png`)
         gameOver()
+    }
+    else {
+        let letterIndexes = []
+        for (let i = 0; i < guessWordArray.length; i++) {
+            if (guessWordArray[i] === key)
+                letterIndexes.push(i)
+        }
+        for (const num of letterIndexes) {
+            const newSecretWord = secretWord.slice(0, num) + guessWordArray[num] + secretWord.slice(num + 1, secretWord.length)
+            secretWord = newSecretWord
+            guessWordDisplay.innerHTML = secretWord
+
+        }
     }
     
 
