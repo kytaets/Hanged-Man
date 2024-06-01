45b23ff0eca9b2691747992ed3a8d52d3a752653
kytaec <vladislavtokovenko4@gmail.com>
2024-06-01 14:01:21 +0300

    added keydown listener

--- script.js
+++ script.js
@@ -4,10 +4,31 @@ import {words} from './words.js'
 const wordsList = words.split('\n').map(word => word.trim()).filter(word => word.length > 0);
 const word = wordsList[Math.floor(Math.random() * wordsList.length)]
 const guessWord = document.getElementById('guess-word')
-const hangmanImg = document.querySelector('#hangman-img')
+
+// getting html elements
+const hangmanImg = document.getElementById('hangman-img')
+const usedLetters = document.getElementById('used-letters')
+
+const guessWordArray = word.split('')
+
 
 let score = 0
+let usedLettersArray = []
 
 guessWord.innerHTML = word
-hangmanImg.setAttribute("src", `./images/hangMan-images/hang-${score}.png`)
 
+
+document.addEventListener("keydown", (event) => {
+    const key = event.code.slice(3,4).toLowerCase()
+    if (usedLettersArray.indexOf(key) === -1){
+        usedLettersArray.push(key)
+        const usedLettersStr = usedLettersArray.join(', ')
+        usedLetters.innerHTML = usedLettersStr
+    }
+    if (guessWordArray.indexOf(key) === -1) {
+        score++
+        hangmanImg.setAttribute("src", `./images/hangMan-images/hang-${score}.png`)
+    }
+    
+
+})
