67ff026c4b21ace897794c1cb6897fcc465941ea
kytaec <vladislavtokovenko4@gmail.com>
2024-06-01 13:29:46 +0300

    managed image src changing; aligning elements

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 import {words} from './words.js'
 
+// choosing a random word
 const wordsList = words.split('\n').map(word => word.trim()).filter(word => word.length > 0);
 const word = wordsList[Math.floor(Math.random() * wordsList.length)]
+const guessWord = document.getElementById('guess-word')
+const hangmanImg = document.querySelector('#hangman-img')
 
 let score = 0
 
-const guess_word = document.getElementById('guess-word')
-guess_word.innerHTML = word
+guessWord.innerHTML = word
+hangmanImg.setAttribute("src", `./images/hangMan-images/hang-${score}.png`)
 
