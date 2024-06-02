35b1cb406ca257d2c4e07a05cc32302254a2de9d
kytaec <vladislavtokovenko4@gmail.com>
2024-06-02 08:19:00 +0300

    added buttons event listeners

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-import {words} from './words.js'
+import {words} from './words.js';
 
 // choosing a random word
 const wordsList = words.split('\n').map(word => word.trim()).filter(word => word.length > 0);
-const word = wordsList[Math.floor(Math.random() * wordsList.length)]
-const guessWordArray = word.split('')
+const word = wordsList[Math.floor(Math.random() * wordsList.length)];
+const guessWordArray = word.split('');
 
 // choosing html elements
-const guessWord = document.getElementById('guess-word')
-const guessWordDisplay = document.getElementById('guess-word-display')
-const hangmanImg = document.getElementById('hangman-img')
-const usedLetters = document.getElementById('used-letters')
+const guessWord = document.getElementById('guess-word');
+const guessWordDisplay = document.getElementById('guess-word-display');
+const hangmanImg = document.getElementById('hangman-img');
+const usedLetters = document.getElementById('used-letters');
+const buttons = document.querySelectorAll("button");
 
-let score = 0
-let usedLettersArray = []
+
+let score = 0;
+let usedLettersArray = [];
 let secretWord = '-'.repeat(word.length);
+
+
+function letterCheck(key) {
+    if (usedLettersArray.indexOf(key) === -1) {
+        usedLettersArray.push(key)
+        usedLetters.innerHTML = usedLettersArray.join(', ')
+
+        if (guessWordArray.indexOf(key) === -1) {
+            score++
+            hangmanImg.setAttribute("src", `./images/hangMan-images/hang-${score}.png`)
+            gameOver()
+        }
+        else {
+            let letterIndexes = []
+            for (let i = 0; i < guessWordArray.length; i++) {
+                if (guessWordArray[i] === key)
+                    letterIndexes.push(i)
+            }
+            for (const num of letterIndexes) {
+                secretWord = secretWord.slice(0, num) + guessWordArray[num] + secretWord.slice(num + 1, secretWord.length)
+                guessWordDisplay.innerHTML = secretWord
+            }
+        }
+    }
+
+}
 
 
 function gameOver() {
@@ -39,32 +67,21 @@ function victory() {
 guessWord.innerHTML = word
 guessWordDisplay.innerHTML = secretWord
 
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const key = button.textContent.toLocaleLowerCase()
+        console.log(key)
+
+        letterCheck(key)
+        victory()
+    }); 
+})
+
+
 document.addEventListener("keydown", (event) => {
     const key = event.code.slice(3,4).toLowerCase()
 
-    if (usedLettersArray.indexOf(key) === -1) {
-        usedLettersArray.push(key)
-        const usedLettersStr = usedLettersArray.join(', ')
-        usedLetters.innerHTML = usedLettersStr
-
-        if (guessWordArray.indexOf(key) === -1) {
-            score++
-            hangmanImg.setAttribute("src", `./images/hangMan-images/hang-${score}.png`)
-            gameOver()
-        }
-        else {
-            let letterIndexes = []
-            for (let i = 0; i < guessWordArray.length; i++) {
-                if (guessWordArray[i] === key)
-                    letterIndexes.push(i)
-            }
-            for (const num of letterIndexes) {
-                const newSecretWord = secretWord.slice(0, num) + guessWordArray[num] + secretWord.slice(num + 1, secretWord.length)
-                secretWord = newSecretWord
-                guessWordDisplay.innerHTML = secretWord
-            }
-        }
-    }
-
+    letterCheck(key)
     victory()
 })
+
